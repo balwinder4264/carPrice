311d1ed8119e70c997125c852a63eb8f05d92af0
balwinder4264 <balwindersingh4264@gmail.com>
2021-11-13 12:36:54 -0500

    nest js

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,30 +1,57 @@
-import { Body,Controller,Post,Get, Param, Query,Delete, Patch,NotFoundException,UseInterceptors,ClassSerializerInterceptor } from '@nestjs/common';
+import { Body,Controller,Post,Get, Param, Query,Delete,
+Patch,NotFoundException,
+Session,
+UseGuards
+} from '@nestjs/common';
 import {CreateUserDto} from "./dtos/create-user.dto"
 import {UsersService} from "./users.service";
 import {UpdateUserDto} from "./dtos/update-user.dto"
 import {Serialize} from "../interceptors/serialize.interceptor"
 import { UserDto } from './dtos/user.dto';
 import {AuthService} from "./auth.service"
+import {CurrentUser} from "./decorators/current-user.decorator";
+
+import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 @Controller('auth')
 //no bussiness logic
 //just apis
 @Serialize(UserDto)
+
 export class UsersController {
     constructor(
         private userService:UsersService,
         private authService:AuthService
-        ){}
+    ){}
+
+    // @Get('/whoami')
+    // whoAmI(@Session() session:any){
+    //     return this.userService.findOne(session.userId)
+    // }
+    @Get('/whoami')
+    @UseGuards(AuthGuard)
+    whoAmI(@CurrentUser() user:User){
+        return user;
+    }
+    @Post('/signout')
+    signOut(@Session() session:any){
+        session.userId = null;
+        // return this.userService.findOne(session.userId)
+    }
     @Post("/signup")
-    createUser(@Body() body:CreateUserDto){
-        return this.authService.signup(body.email,body.password)
+   async createUser(@Body() body:CreateUserDto,@Session() session :any){
+        const user= await this.authService.signup(body.email,body.password)
+        session.userId = user.id;
+        return user;
     }
     @Post("/signin")
-    signin(@Body() body:CreateUserDto){
-        return this.authService.signin(body.email,body.password)
+   async signin(@Body() body:CreateUserDto,@Session() session :any){
+        const user = await this.authService.signin(body.email,body.password);
+        session.userId = user.id;
+        return user;
     }
     @Get('/:id')
        async findUser(@Param('id') id:string){
-           console.log("Handler is running")
         const user =await this.userService.findOne(parseInt(id));
         if(!user){
             throw new NotFoundException('User Not Found');
